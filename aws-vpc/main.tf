0c3916408bba83c9be5d577c81430632aede807c
Diogo <diogo@Note-Home.bbrouter>
2024-09-01 13:26:05 -0300

    criação de uma vpc

---
+++ aws-vpc/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.64.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "aula-terraform-aws-azure"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = local.common_tags
+  }
+}
