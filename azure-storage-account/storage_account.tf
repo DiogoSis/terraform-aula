629ac7f5fed1b0f80b8d44a5bba75863a2b463cb
Diogo <diogo@Note-Home.bbrouter>
2024-09-08 19:51:44 -0300

    feat: readme

--- azure-storage-account/storage_account.tf
+++ azure-storage-account/storage_account.tf
@@ -1,7 +1,7 @@
 resource "azurerm_resource_group" "resource_group" {
   name     = var.resource_group_name
   location = var.location
-  tags     = local.common_tagsy
+  tags     = local.common_tags
 }
 
 resource "azurerm_storage_account" "storage_account" {
