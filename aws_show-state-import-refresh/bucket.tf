9b5b5f730dc89325ba7886948f8cd1f8a64630f6
Diogo <diogo@Note-Home.bbrouter>
2024-09-15 21:36:35 -0300

    aula de show, state, import, refresh

---
+++ aws_show-state-import-refresh/bucket.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "bucket_um" {
+  bucket = "luna-terraform-command-1"
+}
+resource "aws_s3_bucket" "bucket_2" {
+  bucket = "luna-terraform-command-2"
+}
+resource "aws_s3_bucket" "bucket_3" {
+  bucket = "luna-terraform-command-3"
+}
