c2043b845fc2e6709c94debfc67acbac50f34656
Diogo <diogo@Note-Home.bbrouter>
2024-10-10 11:11:23 -0300

    estudo de criação de uma vm usando o  provisioner

---
+++ aws_vm_provisioners/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.65.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "aula-terraform-aws-azure"
+    key    = "aws-vm/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      owner      = "luna"
+      managed-by = "terraform"
+    }
+  }
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "aula-terraform-aws-azure"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
